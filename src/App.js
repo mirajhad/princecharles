c8d7f72c1389c4e7489d9ecaa0acee27699bcf9b
miraj <mirajhadish48@gmail.com>
2023-06-04 02:14:45 +0530

    (feat): maps integrated

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Body from './components/Body/Body';
 import Middle from './components/Middle/Middle';
+import Maps from './components/Maps/Maps';
 function App() {
   return (
        <div className="App">
           <Header />
           <Middle />
           <Body />
+          <Maps />
           <Footer/>
     </div>
   );
